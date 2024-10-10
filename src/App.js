803bd0d69a77caef4367d8549646dad57fdbbbc8
edward <edward_gio@hotmail.com>
2024-10-09 22:09:02 -0500

    Components added - Dashboard, Login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
+    // <Router>
+    //   <Routes>
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/dashboard" element={<Dashboard />} />
+    //     <Route path="*" element={<Navigate to="/login" />} />
+    //   </Routes>
+    // </Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*  */}
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  return (
+    <>
+      <h1>Login component</h1>
+    </>
+  );
+};
+
+export default Login;
