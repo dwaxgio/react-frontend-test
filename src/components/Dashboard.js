dbe20b7e27ef9797eee64e573cfb74405c044c9a
edward <edward_gio@hotmail.com>
2024-10-09 23:02:00 -0500

    Dashboard - Modal adjusted

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -67,13 +67,6 @@ const Dashboard = () => {
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title text-dark">Comment Details</h5>
-                <button
-                  type="button"
-                  className="close"
-                  onClick={handleCloseModal}
-                >
-                  <span>&times;</span>
-                </button>
               </div>
               <div className="modal-body text-dark">
                 {selectedComment && (
